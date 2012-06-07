1bf56e1cff6fcdf19d74de2931484d54bf24cb68
Nick Lloyd <nlloyd@gmail.com>
2012-06-07 13:52:30 -0400

    hooray! awesomeness!

--- sandbox.py
+++ sandbox.py
@@ -2,34 +2,38 @@ import sublime, sublime_plugin
 import threading
 import logging
 import time
+from oyoyo import helpers
 from oyoyo.client import IRCClient 
 from oyoyo.cmdhandler import DefaultCommandHandler  
 
-def connect_callback(cli):
-    # Join the channel '#subliminalcollaborator'
-    time.sleep(10)
-    helpers.join(cli, "#subliminalcollaborator")
+# def connect_callback(cli):
+#     # Join the channel '#subliminalcollaborator'
+#     helpers.join(cli, "#subliminalcollaborator")
 
 class MyHandler(DefaultCommandHandler):
     def privmsg(self, nick, chan, msg):
         print "%s in %s said: %s" % (nick, chan, msg)
+
+    def welcome(self, *args):
+        print 'we have been welcomed!'
+        print args
+        helpers.join(self.client, "#subliminalcollaborator")
 
 cli = IRCClient(MyHandler, host="irc.pearsoncmg.com", port=6667, nick="subliminal_nick",
          passwd='my9pv',
          connect_cb=None,
          blocking=True)
 
-loop = True
-
 class IRCClientThread(threading.Thread): 
     def __init__(self, irc_client):
+        self.loop = True
         self.client = irc_client
         threading.Thread.__init__(self)
 
     def run(self):
         logging.basicConfig(level=logging.DEBUG)
         conn = self.client.connect()
-        while loop:
+        while self.loop:
 #         print "LOOP"   
          conn.next()
 
@@ -39,8 +43,8 @@ def launch():
     irc_thread.start()
 
 def kill():
-    loop = False
-    irc_thread.join()
+    irc_thread.loop = False
+    irc_thread.join(1)
 
 class IrcSandboxCommand(sublime_plugin.TextCommand):
     def run(self, view):
