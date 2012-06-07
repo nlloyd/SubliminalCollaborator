92fd8ae63a5edc7e6d1159ac512fd1079080fce5
Nick Lloyd <nlloyd@gmail.com>
2012-06-07 10:22:18 -0400

    removed oyoyo for real and references

--- session.py
+++ session.py
@@ -22,41 +22,41 @@
 import sublime, sublime_plugin
 import threading
 import logging
-from oyoyo.client import IRCClient
-from oyoyo.cmdhandler import DefaultCommandHandler
-from oyoyo import helpers
+# from oyoyo.client import IRCClient
+# from oyoyo.cmdhandler import DefaultCommandHandler
+# from oyoyo import helpers
 
-def connect_callback(cli):
-    # Identify to nickserv
-    helpers.identify(cli, "my9pv")
+# # def connect_callback(cli):
+# #     # Identify to nickserv
+# #     helpers.identify(cli, "my9pv")
 
-    # Join the channel '#test'
-    helpers.join(cli, "#subliminalcollaborator")
+# #     # Join the channel '#test'
+# #     helpers.join(cli, "#subliminalcollaborator")
 
-class MyHandler(DefaultCommandHandler):
-    # Handle messages (the PRIVMSG command, note lower case)
-    def privmsg(self, nick, chan, msg):
-        print "%s in %s said: %s" % (nick, chan, msg)
+# class MyHandler(DefaultCommandHandler):
+#     # Handle messages (the PRIVMSG command, note lower case)
+#     def privmsg(self, nick, chan, msg):
+#         print "%s in %s said: %s" % (nick, chan, msg)
 
-cli = IRCClient(MyHandler, host="irc.pearsoncmg.com", port=6667, nick="subliminal_nick",
-                connect_cb=connect_callback)
+# cli = IRCClient(MyHandler, host="irc.pearsoncmg.com", port=6667, nick="subliminal_nick",
+#                 connect_cb=None)
 
-class IRCClientThread(threading.Thread):
-    def __init__(self, irc_client):
-        self.client = irc_client
-        threading.Thread.__init__(self)
+# class IRCClientThread(threading.Thread):
+#     def __init__(self, irc_client):
+#         self.client = irc_client
+#         threading.Thread.__init__(self)
 
-    def run(self):
-        logging.basicConfig(level=logging.DEBUG)
-        conn = self.client.connect()
-        while True:
-            conn.next()
+#     def run(self):
+#         logging.basicConfig(level=logging.DEBUG)
+#         conn = self.client.connect()
+#         while True:
+#             conn.next()
 
-irc_thread = IRCClientThread(cli)
-irc_thread.start()
+# irc_thread = IRCClientThread(cli)
+# irc_thread.start()
 
 class CollabSessionCommand(sublime_plugin.WindowCommand):
     def run(self):
-        print cli
-        helpers.msg(cli, 'nick_hackathon', "ohai!")
+        print 'blah blah blah'
+        # helpers.msg(cli, 'nick_hackathon', "ohai!")
 
