6a40e098cbb9ae58f2161d2bd9ecce0262aaf6cb
Nick Lloyd <nlloyd@gmail.com>
2012-06-08 19:29:04 -0400

    cleanup

--- session.py
+++ session.py
@@ -218,9 +218,9 @@ class IRCClientThread(threading.Thread):
 # {
 #     "subliminal_collaborator_config": {
 #         "irc": {
-#             "host": "irc.pearsoncmg.com",
+#             "host": "irc.something.com",
 #             "port": 6667,
-#             "pwd": "my9pv",
+#             "pwd": "somepwd",
 #             "nick": "subliminal_nick"
 #         }
 #     }
@@ -241,14 +241,7 @@ class CollabSessionCommand(sublime_plugin.WindowCommand):
             self.irc_client = None
             self.irc_thread = None
 
-        collab_config = self.window.active_view().settings().get("subliminal_collaborator_config", {
-                "irc": {
-                    "host": "irc.pearsoncmg.com",
-                    "port": 6667,
-                    "pwd": "my9pv",
-                    "nick": "sub_nick"
-                }
-            })
+        collab_config = self.window.active_view().settings().get("subliminal_collaborator_config", None)
         irc_host = collab_config['irc']['host']
         irc_port = collab_config['irc']['port']
         irc_pwd = collab_config['irc']['pwd']
